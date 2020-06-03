57d0a96e6d7b5735f7d284e45461d041f1e6d5ad
siyam04 <galib.abdullah04@gmail.com>
2020-06-03 13:12:24 +0600

    API: 1, 2, 3, 13 cleaned

--- jobs/forms.py
+++ jobs/forms.py
@@ -8,10 +8,27 @@ from .models import (
 )
 
 
+# Category Form
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+
+
+# Job Form
+class JobForm(forms.ModelForm):
+    class Meta:
+        model = Job
+        fields = '__all__'
+
+
+# JobTracking Form
 class JobTrackingForm(forms.ModelForm):
     class Meta:
         model = JobTracking
         fields = ['job', 'seeker_id', 'seeker_name']
+
+
 
 
 
--- jobs/views.py
+++ jobs/views.py
@@ -19,10 +19,10 @@ from .models import (
     Category,
     JobTracking,
 )
-from .forms import JobTrackingForm
+from .forms import CategoryForm, JobForm, JobTrackingForm
 
 
-# Static APIs: http://127.0.0.1:8000/api/choices/
+# 0. Static APIs: http://127.0.0.1:8000/api/choices/
 @method_decorator(csrf_exempt, name='dispatch')
 class StaticChoiceView(View):
     def get(self, request):
@@ -34,7 +34,6 @@ class StaticChoiceView(View):
                 "resume_receiving_option": dict(Job.ResumeReceivingOption.choices)
             }
         ]
-
         return JsonResponse({"data": data}, status=200)
 
 
@@ -44,13 +43,25 @@ class StaticChoiceView(View):
 # 13. Apply Job: http://127.0.0.1:8000/api/job/apply/id/ (POST)
 @method_decorator(csrf_exempt, name='dispatch')
 class JobView(View):
-    job_fields = ['id', 'job_title', 'no_of_vacancies', 'category']
-    list_querystring = ['no_of_vacancies', 'category']
-    list_details = ['age', 'category']
+    job_fields = [
+        'job_title', 'job_level', 'job_responsibilities', 'job_location', 'no_of_vacancies', 'category',
+        'employer_id', 'employer_name', 'employer_information', 'employment_status', 'employer_location',
+        'age', 'gender', 'skill', 'experience', 'training', 'salary', 'compensation_and_other_benefits',
+        'application_deadline', 'resume_receiving_option'
+    ]
+    job_fields_with_id = [
+        'id', 'job_title', 'job_level', 'job_responsibilities', 'job_location', 'no_of_vacancies', 'category',
+        'employer_id', 'employer_name', 'employer_information', 'employment_status', 'employer_location',
+        'age', 'gender', 'skill', 'experience', 'training', 'salary', 'compensation_and_other_benefits',
+        'application_deadline', 'resume_receiving_option'
+    ]
     job_tracking_fields = ['seeker_id', 'seeker_name', 'job']
+    job_list_querystring = ['no_of_vacancies', 'category', 'employer_id', 'age', 'gender']
 
     # 1
+    # 13
     def post(self, request, id=None, *args, **kwargs):
+        # 13. Apply Job
         if id:
             body_unicode = request.body.decode('utf-8')
             body = json.loads(body_unicode)
@@ -60,126 +71,52 @@ class JobView(View):
 
             if form.is_valid():
                 instance = form.save()
-
-                return JsonResponse(model_to_dict(instance, fields=self.job_tracking_fields))
+                return JsonResponse(model_to_dict(instance, fields=self.job_tracking_fields), status=201)
                 # return JsonResponse(model_to_dict(instance, fields=[field.name for field in instance._meta.fields]))
                 # return JsonResponse(model_to_dict(instance, fields=["seeker_name", "seeker_id"]))
             else:
-                return JsonResponse({"errors": form.errors}, status=402)
+                return JsonResponse({"errors": form.errors}, status=422)
 
+        # 1. Create Job
         else:
-            # ToDo: ModelForm validation, params spreading for getting body data and create(), prevent duplicate obj creation
-            # ToDo: master testing
-            # parsing body data
             body_unicode = request.body.decode('utf-8')
             body = json.loads(body_unicode)
 
-            # assigning parsed data
-            job_title = body['job_title']
-            job_level = body['job_level']
-            job_responsibilities = body['job_responsibilities']
-            job_location = body['job_location']
-            no_of_vacancies = body['no_of_vacancies']
+            form = JobForm(body)
 
-            # getting category object by body data
-            try:
-                category_id = json.dumps(body['category'])
-                category = Category.objects.get(id=category_id)
-            except Exception as e:
-                return JsonResponse({"message": e})
-
-            employer_id = body['employer_id']
-            employer_name = body['employer_name']
-            employer_information = body['employer_information']
-            employment_status = body['employment_status']
-            employer_location = body['employer_location']
-
-            age = body['age']
-            gender = body['gender']
-            skill = body['skill']
-            experience = body['experience']
-            # degree = body['degree']
-            training = body['training']
-            salary = body['salary']
-            compensation_and_other_benefits = body['compensation_and_other_benefits']
-
-            application_deadline = body['application_deadline']
-            resume_receiving_option = body['resume_receiving_option']
-
-            # if data available
-            if job_title and job_level and job_responsibilities and job_location and no_of_vacancies and category and employer_id and employer_name and employer_information and employment_status and employer_location and age and gender and skill and experience and training and salary and compensation_and_other_benefits and application_deadline and resume_receiving_option:
-
-                # body_params = [
-                #     job_title, job_level, job_responsibilities, job_location, no_of_vacancies,
-                #     employer_id, employer_name, employer_information, employment_status, employer_location,
-                #     age, gender, skill, experience, training, salary, compensation_and_other_benefits,
-                #     application_deadline, resume_receiving_option
-                # ]
-
-                # create job
-                Job.objects.create(
-                    job_title=job_title,
-                    job_level=job_level,
-                    job_responsibilities=job_responsibilities,
-                    job_location=job_location,
-                    no_of_vacancies=no_of_vacancies,
-
-                    category=category,
-
-                    employer_id=employer_id,
-                    employer_name=employer_name,
-                    employer_information=employer_information,
-                    employment_status=employment_status,
-                    employer_location=employer_location,
-
-                    age=age,
-                    gender=gender,
-                    skill=skill,
-                    experience=experience,
-                    # degree=degree,
-                    training=training,
-                    salary=salary,
-                    compensation_and_other_benefits=compensation_and_other_benefits,
-
-                    application_deadline=application_deadline,
-                    resume_receiving_option=resume_receiving_option,
-                )
-
-                # return api response
-                return JsonResponse({'message': 'Job Created!'}, status=201, safe=False)
-
-            # if data not available
+            if form.is_valid():
+                instance = form.save()
+                return JsonResponse(model_to_dict(instance, fields=self.job_fields), status=201)
             else:
-                return JsonResponse({"message": "Not Found!"}, status=404, safe=False)
+                return JsonResponse({"errors": form.errors}, status=422)
 
     # 2
     # 3
     def get(self, request, id=None):
-        queryset = Job.objects
+        jobs = Job.objects
 
+        # 3. Job Details
         if id:
-            job_detail = queryset.get(id=id)
-            data = {
-                "id": job_detail.id,
-                "job_title": job_detail.job_title,
-                "job_level": job_detail.job_level,
-                "job_responsibilities": job_detail.job_responsibilities,
-            }
+            query = jobs.get(id=id)
+            return JsonResponse(model_to_dict(query, fields=self.job_fields_with_id), status=200)
+
+        # 2. Job List
         else:
-            # list search
-            job_list = queryset.values(*self.list_fields)
-            data = list(job_list)
+            # Normal
+            # query = jobs.values(*self.job_fields_with_id)
+            # data = list(query)
 
             # QS
-            ## job_list = queryset.values(*self.list_querystring)
-            # job_list = queryset.values(*self.list_fields)
-            # no_of_vacancies = request.GET.get('no_of_vacancies')
-            # category = request.GET.get('category')
-            # jobs = list(job_list.filter(no_of_vacancies=no_of_vacancies, category=category))
-            # data = list(jobs)
+            data = {}
+            fields = jobs.values(*self.job_fields_with_id)
+            for field in self.job_list_querystring:
+                data.update({field: request.GET.get(field)})
+
+            jobs = list(fields.filter(**data))
+            data = list(jobs)
 
             # pagination
-            paginator = Paginator(data, 5)
+            paginator = Paginator(data, 3)
             page_number = request.GET.get('page')
             page_obj = paginator.get_page(page_number)
 
@@ -194,8 +131,6 @@ class JobView(View):
             }
 
             return JsonResponse(context)
-
-        return JsonResponse({"data": data}, status=200)
 
 
 # 4. Edit Job : http://127.0.0.1:8000/api/job/id/ (PUT)
